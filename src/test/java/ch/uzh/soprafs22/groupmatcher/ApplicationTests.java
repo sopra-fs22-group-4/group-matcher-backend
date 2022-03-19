301f712df06ba4af914fa60e255a081eda5f2881
Valentine <hzvalentine@gmail.com>
2022-03-19 20:46:27 +0100

    Update contextLoads tests

--- src/test/java/ch/uzh/soprafs22/groupmatcher/ApplicationTests.java
+++ src/test/java/ch/uzh/soprafs22/groupmatcher/ApplicationTests.java
@@ -1,13 +1,25 @@
 package ch.uzh.soprafs22.groupmatcher;
 
+import ch.uzh.soprafs22.groupmatcher.controller.MatcherController;
+import ch.uzh.soprafs22.groupmatcher.model.MatcherDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    MatcherController matcherController;
+
     @Test
     void contextLoads() {
+        MatcherDTO newMatcher = new MatcherDTO();
+        newMatcher.setName("test");
+        matcherController.createMatcher(newMatcher);
+        assertEquals("test", matcherController.getMatcherByPartialName("test").getName());
     }
 
 }
